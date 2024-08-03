fa78451562b745130309a90e45887167f8698e43
chirag2103 <chirag21032004@gmail.com>
2024-08-03 18:26:42 +0530

    process.env added

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import connectDb from './config/database.js';
 import app from './app.js';
 dotenv.config({ path: './config/config.env' });
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 connectDb();
 
 app.listen(port, (req, res) => {
